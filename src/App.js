8c19dec47a9ffc57ed2a2314d49684174dba5693
Alexander Korobov <dktwho@mail.ru>
2022-09-06 11:28:29 +0300

    React Counter on calss component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import ClassComponent from './ClassComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ClassComponent/>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
